d1d3d5dbb24d1b14534f088a1f5256f231d7cab2
sankalpsingh <sankalpkumar.singh60@gmail.com>
2025-11-28 00:07:08 +0530

    feat: integrate custom OkHttp client with NetworkToolsManager in MainApplication of example app

--- example/android/app/src/main/java/networktools/example/MainApplication.kt
+++ example/android/app/src/main/java/networktools/example/MainApplication.kt
@@ -5,14 +5,17 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.modules.network.NetworkingModule
+import com.networktools.NetworkToolsManager
+import okhttp3.OkHttpClient
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost =
+
+  override val reactNativeHost: DefaultReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
@@ -26,6 +29,7 @@ class MainApplication : Application(), ReactApplication {
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+
       }
 
   override val reactHost: ReactHost
@@ -33,6 +37,16 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
+
+    NetworkingModule.setCustomClientBuilder(
+      object : NetworkingModule.CustomClientBuilder {
+        override fun apply(builder: OkHttpClient.Builder) {
+          NetworkToolsManager.addInterceptor(builder)
+        }
+      }
+    )
+
     loadReactNative(this)
   }
 }
+
